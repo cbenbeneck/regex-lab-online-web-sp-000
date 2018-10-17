160a571dc9f34b18dcc6557fc9c0b9380b4b6314
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-10-17 15:09:16 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,8 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.scan(/\Aun/) && if text.scan(/\zing/)
-    true
+  if text.scan(/un+w\ing/)
   else
     false
   end
