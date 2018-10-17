18d647f2d33ddfb8ff2183ea2fac0f23df2d6f9b
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-10-17 15:22:28 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,10 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\w/) 
-
-
-
+  if text.match(/^[A-Z]\w/) 
 end
 
 def valid_phone_number?(phone)
