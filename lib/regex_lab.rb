76f632b76dbc0ec39bf8b3e41e3fbb6f3bff0046
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-10-17 15:06:02 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,9 +7,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if.text.match(/\un/) 
-
-
+  if.text.match(/\A(un)/) && if text.match(/\z(ing)/)
+    true
+  else
+    false
+  end
 end
 
 def words_five_letters_long(text)
